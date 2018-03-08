3160e72f80199f88b02ded9c996c5143c80154d1
prithvinambiar <prithvinambiar@gmail.com>
2018-03-08 22:54:11 +0530

    Added bootstrap and updated spec files

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('mlservice App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to Machine learning as a service!');
   });
 });
